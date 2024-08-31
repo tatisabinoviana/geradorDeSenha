f5e41c0607a2f15411059dc0552133cfc204a414
Tatiana <tatisabinoviana@gmail.com>
2024-08-31 18:50:07 -0300

    commit inicial

---
+++ index.js
@@ -0,0 +1,9 @@
+const range = document.querySelector('#range'),
+  progressbar = document.querySelector('.progress-bar');
+
+range.addEventListener('input', function () {
+  const value = range.value;
+  progressbar.style.setProperty('--progress', value);
+});
+
+const gerador = 'Gerador de senhas';
